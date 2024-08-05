64549d8a7affd13a12985fa2da2ea33a565a78c5
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2024-08-04 21:00:03 -0300

    criado um conversor de tipos

--- praticas/fundamental/1-hello-world.js
+++ praticas/fundamental/1-hello-world.js
@@ -1,6 +1,8 @@
 function printMessage(message){
     console.log(message)
 }
+printMessage('Hello World!')
+
 
 const myself = {
     firstName: "Andreas",
@@ -8,6 +10,4 @@ const myself = {
     age: 16,
     mainStack: "JavaScript"
 }
-
-printMessage('Hello World!')
 printMessage(`My name is ${myself.firstName} ${myself.lastName}, i'm ${myself.age} years old, and i'm a ${myself.mainStack} dev!`)
---
+++ praticas/fundamental/2-conversor-tipos.js
@@ -0,0 +1,96 @@
+function intToFloat(givenValue){
+    console.log( '---------- CONVERSÃO DE INTEIRO PARA FLUTUANTE: ----------' )
+
+    try{
+        if( isNaN(givenValue) ){
+            throw new Error( 'O valor passado não é um número!' )
+        }
+        
+        if( !Number.isInteger(givenValue) ) {
+            throw new Error( 'O valor passado não é um inteiro!' )
+        }
+
+        let result = parseFloat(givenValue)
+
+        console.log(`O valor inteiro: ${givenValue} foi convertido para flutuante: ${result.toFixed(2)}!`)
+    } catch(error) {
+        console.log(` [ERRO] ${error.message} `)
+    }
+}
+
+
+
+function floatToInt(givenValue){
+    console.log( '---------- CONVERSÃO DE FLUTUANTE PARA INTEIRO: ----------' )
+
+    try {
+        
+        if(isNaN(givenValue)){
+            throw new Error( 'O valor fornecido não é válido!' )
+        }
+
+        if(Number.isInteger(givenValue)){
+            throw new Error('O valor passado não é um flutuante!')
+        }
+
+        let result = parseInt(givenValue)
+        
+        console.log(`O valor flutuante: ${givenValue} foi convertido para inteiro: ${result} `)
+
+    } catch (error) {
+        console.log(`[ERRO] ${error.message}`)
+    }
+}
+
+
+function numberToString(givenValue){
+    console.log( '---------- CONVERSÃO DE NUMERO PARA STRING: ----------' )
+
+    try {
+        if( isNaN(givenValue) ){
+            throw new Error( 'O valor passado não é um número!' )
+        }
+
+        let result = String(givenValue)
+        console.log( `O número: ${givenValue} foi convertido para string: "${result}"` )
+        console.log( `Seu typeof foi de ${typeof(givenValue)}, para: ${typeof(result)}` )
+    } catch (error) {
+        console.log( `[ERRO] ${error.message}` )
+    }
+}
+
+function stringToNumber(givenValue){
+    console.log( '---------- CONVERSÃO DE STRING PARA NUMERO: ----------' )
+    
+    // tente:
+    try {
+        // resultado é: o valor dado (texto) transformado em numero:
+        let result = Number(givenValue)
+
+        // se o valor dado (texto) não contém números, mostre esse erro:
+        if(isNaN(givenValue)){
+            throw new Error( 'Sua string contém caracteres inconversíveis para número!' )
+        }
+
+        // se o resultado não for um número, mostre esse erro:
+        if(isNaN(result)){
+            throw new Error( 'O valor passado não é um número!' )
+        }
+
+        // agora mostra o valor antigo, em forma de string, e o valor novo convertido:
+        console.log( `A string: ${givenValue} foi convertida para número: ${result}` )
+
+        // mostra também que o tipo dos dados foi mudado de texto pra numero!
+        console.log( `Seu typeof foi de ${typeof(givenValue)}, para ${typeof(result)}` )
+
+      // se não der, faz isso aqui:
+    } catch (error) {
+        console.log( `[ERRO] ${error.message}` )
+    }
+}
+
+
+intToFloat(10)
+floatToInt(82.93)
+numberToString(45)
+stringToNumber('983489.99')
